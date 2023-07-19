99696160bf347eab334bc547191e532821eb2a71
Josh Booker <josh.booker2@gmail.com>
2023-07-19 14:01:33 +0100

    updated event_list

--- models/event_list.py
+++ models/event_list.py
@@ -4,3 +4,5 @@ import datetime
 josh_birthday = Event(datetime.date(2023, 11, 28), "Josh's Birthday", 30, "Josh's House", "Josh's big birthday party extravaganza")
 hogmanay = Event(datetime.date(2023, 12, 31), "Hogmanay", 100, "Princes Street", "Hogmanay Street Party")
 halloween = Event(datetime.date(2023, 10, 31), "Halloween", 30, "Edinburgh Castle", "Halloween party at the castle")
+
+event_list = [josh_birthday, hogmanay, halloween]
