b1ba4b87d6e2ccbc3d046ecad6becbbd767f411f
Josh Booker <josh.booker2@gmail.com>
2023-07-19 14:00:40 +0100

    added event class and event_list

---
+++ models/event.py
@@ -0,0 +1,8 @@
+class Event:
+
+    def __init__(self, date, name, guest_capacity, location, description):
+        self.date = date
+        self.name = name
+        self.guest_capacity = guest_capacity
+        self.location = location
+        self.description = description
